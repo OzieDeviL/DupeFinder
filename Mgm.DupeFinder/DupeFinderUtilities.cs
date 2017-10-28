420018c6fab1a22d2b2567fb9732b73af38d464b
Development <development@Davids-MacBook-Pro.local>
2017-10-28 14:28:14 -0700

    Initial Working App in Debug Mode

---
+++ Mgm.DupeFinder/DupeFinderUtilities.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Data.HashFunction;
+using System.Text;
+using System.Linq;
+
+namespace Mgm.DupeFinder
+{
+    public static class DupeFinderUtilities
+    {
+        public static void FileCollector(string parentDir, List<string> allFiles)
+        {
+            //this method retrieves file names with their full path
+            List<string> dirFiles = Directory.EnumerateFiles(parentDir).ToList();
+            //add that to the list of files
+            allFiles.AddRange(dirFiles);
+            List<string> subDirs = Directory.EnumerateDirectories(parentDir).ToList();
+            if (subDirs.Count() > 0)
+            {
+                foreach (string subDirectory in subDirs)
+                {
+                    FileCollector(subDirectory
+                                  , allFiles);
+                }
+            }
+        }
+
+        public static void GroupByHash(Dictionary<string, List<string>> fileGroups
+                                       , Dictionary<string, List<string>> nonUniqueSizes)
+        {
+            foreach (KeyValuePair<string, List<string>> uniqueSize in nonUniqueSizes)
+            {
+                //create a small dictionary to hold the hashed entries
+                Dictionary<string, List<string>> uniqueHashes = new Dictionary<string, List<string>>();
+                //hash a file
+                foreach (string fileName in uniqueSize.Value)
+                {
+                    string entryHash = FileHasher(fileName);
+                    if (uniqueHashes.ContainsKey(entryHash))
+                    {
+                        uniqueHashes[entryHash].Add(fileName);
+                    }
+                    else
+                    {
+                        List<string> newUniqueHashEntry = new List<string>();
+                        newUniqueHashEntry.Add(fileName);
+                        uniqueHashes.Add(entryHash, newUniqueHashEntry);
+                    }
+                }
+                foreach (KeyValuePair<string, List<string>> uniqueHash in uniqueHashes) {
+                    //don't want to convert the byte array, might be big, so just use a GUID
+                    fileGroups.Add((Guid.NewGuid()).ToString(), uniqueHash.Value);
+                }
+                fileGroups.Remove(uniqueSize.Key);
+            }
+        }
+
+        public static void ConsoleLogFileGroups(List<List<string>> fileGroups, string rootDir)
+        {
+            Console.Write(Environment.NewLine + "------------------------RESULTS------------------------------------------"
+                          + Environment.NewLine + "Files bracketed together have the same content (their metadata may vary)."
+                          + Environment.NewLine + "All files are listed relative to the directory: " + rootDir
+                          + Environment.NewLine + "NOTE: The list below will include hidden system files such as .Ds_Store that won't show up in Finder, don't worry about them");
+            foreach (List<string> fileGroup in fileGroups)
+            {
+                fileGroup.Sort();
+                Console.Write(Environment.NewLine 
+                              + "[");
+                for (int i = 0; i < fileGroup.Count(); i++)
+                    if (i < fileGroup.Count() - 1)
+                    {
+                    Console.Write(Environment.NewLine 
+                                  + "\t" 
+                                  + Path.GetRelativePath(rootDir, fileGroup[i]));
+                    } else
+                    {
+                    Console.Write(Environment.NewLine 
+                                  + "\t" + Path.GetRelativePath(rootDir, fileGroup[i]) 
+                                  + Environment.NewLine 
+                                  + "],");
+                    }
+            }
+            Console.Write("\b" + Environment.NewLine + "--------------------------END RESULTS------------------------------");
+        }
+
+        private static string FileHasher(string fileName)
+        {   
+            xxHash xh = new xxHash();
+            using (FileStream fs = File.Open(fileName, FileMode.Open))
+            {
+                byte[] byteHash = xh.ComputeHash(fs);
+                string stringHash = BitConverter.ToString(byteHash);
+                return stringHash;
+            }
+        }
+
+        public static void UniqueKeyChecker(string candidateKey
+                                            , string candidateFileName
+                                             , Dictionary<string, List<string>> uniques)
+        {
+            //check if there are dupes already caught
+            if (uniques.ContainsKey(candidateKey)) {
+                uniques[candidateKey].Add(candidateFileName);    
+            } else {
+                List<string> newUniquesEntry = new List<string>();
+                newUniquesEntry.Add(candidateFileName);
+                uniques.Add(candidateKey, newUniquesEntry);
+            }
+        }
+
+        public static Dictionary<string, List<string>> NonUniquesQuery(Dictionary<string, List<string>> groupedFiles)
+        {
+            return 
+                (from entry in groupedFiles
+                 where entry.Value.Count() > 1
+                 select new { entry.Key, entry.Value })
+                    .ToDictionary(x => x.Key, x => x.Value);
+            
+        }   
+    }
+}
---
+++ Mgm.DupeFinder/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Mgm.DupeFinder
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string rootDir = null;
+            Console.WriteLine("Enter the directory you want to search in this format: /[yourUserName]/Path/To/Directory, or drag and drop the folder from a finder window onto the line below:");
+            rootDir = Console.ReadLine().Trim();
+            //Create a uniques dictionary
+            Dictionary<string, List<string>> fileGroups = new Dictionary<string, List<string>>();
+            //Collect all the files
+            List<string> allFiles = new List<string>();
+            DupeFinderUtilities.FileCollector(rootDir, allFiles);
+
+            //group by fileSize
+            foreach (string fileName in allFiles)
+            {
+                //test for unique file size
+                FileInfo fileInfo = new FileInfo(fileName);
+                string fileSize = fileInfo.Length.ToString();
+                DupeFinderUtilities.UniqueKeyChecker(fileSize
+                                  , fileName
+                                  , fileGroups);
+            }
+            //query for multi-file groupings
+            Dictionary<string, List<string>> nonUniqueSizes = DupeFinderUtilities.NonUniquesQuery(fileGroups);
+            //group by hashes
+            DupeFinderUtilities.GroupByHash(fileGroups, nonUniqueSizes);
+            //sort to put non-uniques at the top of a list
+            List<List<string>> uniqueContents =
+                (from fileGroup in fileGroups
+                 orderby fileGroup.Value.Count descending
+                 select fileGroup.Value)
+                    .ToList();
+
+            DupeFinderUtilities.ConsoleLogFileGroups(uniqueContents, rootDir);
+            //dump all the entries with 0 Count values
+            //sort entries by value count
+            //group by fileHash
+        }
+        //grab subdirectories 
+        //grab files
+        //Search the files for duplicate weights
+        //hash the files with duplicated weights
+        //search the duplicated weights for duplicate hashes
+    }
+}
+
+
+
